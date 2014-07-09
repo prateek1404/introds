237e75ccd4407c0299a12b74a02aa59218ef8388
Prateek Jain <prateek1404@gmail.com>
2014-07-10 01:33:25 +0530

    first assignment completed

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -3,23 +3,29 @@ import json
 
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    scores = {}
-    for line in sent_file:
-     term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-     scores[term] = int(score)  # Convert the score to an integer.
-    count =0;
-    scoreOfTweet = 0
+    tweet_file = open(sys.argv[1])
+    hashCount={}
     for li in tweet_file:
      data = json.loads(li);
-     if "text" in data.keys():
-       words = data["text"].encode('utf-8').split(" ");
-       for word in words:
-        if word in scores.keys():
-         scoreOfTweet = scoreOfTweet + int(scores[word])
-     print scoreOfTweet
-     scoreOfTweet = 0
-         
+     if "entities" in data.keys():
+       entities = data['entities']
+       if "hashtags" in entities.keys():
+        hashtags = entities["hashtags"]
+        for has in hashtags:
+         if has["text"] in hashCount.keys():
+          hashCount[has["text"]] = hashCount[has["text"]]+1
+         else:
+          hashCount[has["text"]]=1
+    hashArray=[]     
+    for h in hashCount.keys():
+     if len(hashArray) ==0:
+      hashArray.append({'name':h,'count':int(hashCount[h])})
+     else:
+      k=0
+      while hashCount[h]<hashArray[k]['count'] and k<len(hashArray): 
+       k = k+1
+      hashArray.insert(k,{'name':h,'count':int(hashCount[h])})
+    for x in range(0,10):
+      print hashArray[x]['name'].encode('utf-8')+" "+str(hashArray[x]['count'])
 if __name__ == '__main__':
     main()
